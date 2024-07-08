0350dc097fa326fbc4ece78f43b96a93f378f14a
qznikk <115718297+qznikk@users.noreply.github.com>
2024-07-08 19:34:33 +0200

    simple page

---
+++ main.js
@@ -0,0 +1,8 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.gallery img');
+
+function changeSlide(direction) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
